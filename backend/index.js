378804ef077b98900bde9fdc1b41d54bf86e265b
Ekalavya L <86137780+Ekalavya123@users.noreply.github.com>
2024-01-30 12:10:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   })
   
-app.use('',require("./Routes/Fetching"));
+app.use(require("./Routes/Fetching"));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
