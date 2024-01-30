2df640ec303cd82e8dea8a5a97971f7c9a706afc
Ekalavya L <86137780+Ekalavya123@users.noreply.github.com>
2024-01-30 13:12:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cors())
 app.use(cors(
     {
-        origin:["http://localhost:3000"],
+        origin:["https://mnnitian.vercel.app"],
         methods:["POST","GET"],
         credentials:true
     }
