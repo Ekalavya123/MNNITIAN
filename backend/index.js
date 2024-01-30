f922a7e6d8fb42eeab3277485f76eb00102f236c
Ekalavya L <86137780+Ekalavya123@users.noreply.github.com>
2024-01-30 12:42:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ const mongoDB = require("./db");
 
 
 var cors = require('cors')
+app.use(express.json())
+app.use(cors())
 app.use(cors(
     {
         origin:["http://localhost:3000"],
@@ -13,7 +15,7 @@ app.use(cors(
         credentials:true
     }
 ))
-app.use(express.json())
+
 mongoDB();
 
 app.get('/', (req, res) => {
